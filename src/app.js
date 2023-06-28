aa4fd32c8ad0b21058579d143f1a25ffd101bd24
Eunice Gallardo <128846071+Eunicegallardo@users.noreply.github.com>
2023-06-28 05:58:35 +0000

    Random card generator finished

--- src/app.js
+++ src/app.js
@@ -5,7 +5,35 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector(".card").classList.add(generateRandomCards());
+  document.querySelector(".card").innerHTML = generateRandomValue();
+};
+
+let generateRandomValue = () => {
+  let value = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let randomValue = Math.floor(Math.random() * value.length);
+
+  return value[randomValue];
+};
+
+let generateRandomCards = () => {
+  let cards = ["heart", "spade", "club", "diamond"];
+  let randomCard = Math.floor(Math.random() * cards.length);
+
+  return cards[randomCard];
 };
